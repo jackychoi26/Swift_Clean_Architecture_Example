b6bbdb3a5419d407475529ea6ba7b98619286393
jackychoi26 <gmu1233@gmail.com>
2024-06-10 15:05:14 +0800

    Initial Commit

---
+++ Swift-Clean-Architecture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift-Clean-Architecture
+//
+//  Created by Choi Kin Lung on 10/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift-Clean-Architecture/Swift_Clean_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Clean_ArchitectureApp.swift
+//  Swift-Clean-Architecture
+//
+//  Created by Choi Kin Lung on 10/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Clean_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
